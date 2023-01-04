a597c7acf4808628581fdfd695ac1f22e77349ce
Francois Jordaan <francois-fuse@Francois-Jordaan-MacBook-Pro.local>
2023-01-04 09:23:08 +0200

    Fixed tests and added types for routes

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -13,30 +13,39 @@ export interface ImageProperties {
 images.use(express.static('src/assets/source-images'))
 images.use(express.static('src/assets/resized-images'))
 
-images.get('/', imageUtils.imageCache, async (req, res) => {
-  try {
-    const { filename, height, width }: ImageProperties = req.query
-
-    if (filename === undefined || width === undefined || height === undefined) {
-      res.send('Please provide a file name, width and heigt for the image')
-    } else if (
-      Number(width) <= 0 ||
-      Number(height) <= 0 ||
-      isNaN(Number(width)) ||
-      isNaN(Number(height))
-    ) {
-      res.send(
-        'Unable to process the image resizing. Please provide a height and width as a numerical value larger than 0.'
-      )
-    } else if (await imageUtils.findSourceImage(filename as string)) {
-      await resizeImage(String(filename), Number(width), Number(height))
-      res.send(`<img src=${filename}_${width}x${height}.jpeg alt=${filename}>`)
-    } else {
-      res.send(`Sorry, there is no image with a file name of ${filename}`)
+images.get(
+  '/',
+  imageUtils.imageCache,
+  async (req: express.Request, res: express.Response): Promise<void> => {
+    try {
+      const { filename, height, width }: ImageProperties = req.query
+      if (
+        filename === undefined ||
+        width === undefined ||
+        height === undefined
+      ) {
+        res.send('Please provide a file name, width and heigt for the image')
+      } else if (
+        Number(width) <= 0 ||
+        Number(height) <= 0 ||
+        isNaN(Number(width)) ||
+        isNaN(Number(height))
+      ) {
+        res.send(
+          'Unable to process the image resizing. Please provide a height and width as a numerical value larger than 0.'
+        )
+      } else if (await imageUtils.findSourceImage(filename as string)) {
+        await resizeImage(String(filename), Number(width), Number(height))
+        res.send(
+          `<img src=${filename}_${width}x${height}.jpeg alt=${filename}>`
+        )
+      } else {
+        res.send(`Sorry, there is no image with a file name of ${filename}`)
+      }
+    } catch (err) {
+      console.error(err)
     }
-  } catch (err) {
-    console.error(err)
   }
-})
+)
 
 export default images
--- src/tests/utilities/sharpImageResizerSpec.ts
+++ src/tests/utilities/sharpImageResizerSpec.ts
@@ -1,14 +1,15 @@
 import resizeImage from '../../utilities/sharpImageResizer'
+import path from 'path'
 
 describe('resizeImage', () => {
   it("should return an error if the file name provided doesn't exist in the src/assets/source-images directory", async () => {
+    const imagePath = path.join('src', 'assets', 'source-images', 'boo.jpeg')
     expect(await resizeImage('boo', 100, 100)).toEqual(
-      new Error('Input file is missing: src/assets/source-images/boo.jpeg')
+      new Error(`Input file is missing: ${imagePath}`)
     )
   })
   it('should return a relative path of the source image if the file name provided exists in the src/assets/souce-images directory', async () => {
-    expect(await resizeImage('fjord', 100, 100)).toEqual(
-      'src/assets/source-images/fjord'
-    )
+    const imagePath = path.join('src', 'assets', 'source-images', 'fjord')
+    expect(await resizeImage('fjord', 100, 100)).toEqual(imagePath)
   })
 })
